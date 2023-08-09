aa0a8e886b85c9747506cca6e134c57f4e4eb87d
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-08-09 06:03:32 +0100

    updated source code

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -3,5 +3,5 @@ i = 1
 for char in range (ord('z'), ord('a') - 1, -1):
     if (i % 2 == 0):
         char = char - 32
-    print(chr(char), end='')
+    print("{}".format(chr(char)), end='')
     i = i + 1
