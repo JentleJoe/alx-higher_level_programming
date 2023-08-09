5b1224d44dc2ea620001fbb7661db018ad94cd55
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-08-09 02:41:53 +0100

    updated source code

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,7 +3,7 @@ start = 0
 end = 0
 while (start <= 9):
     if (start == 9 and end == 9):   # prints last number with newline
-        print("{}{}\n".format(start, end))
+        print("{}{}".format(start, end))
         break
 
     print("{}{}, ".format(start, end), end="")
