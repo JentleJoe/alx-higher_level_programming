a0fe396c8951c798804883c84ec9cd4a5a479a0f
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-08-09 03:54:04 +0100

    added new file

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+start = 0
+end = 0
+for start in range(0, 9):
+    for end in range(1, 10):
+        if (start == 8 and end == 9):   # prints last number with newline
+            print("{}{}".format(start, end))
+            break
+        if (start < end):
+            print("{}{} ,".format(start, end), end='')
+        elif (start == end):
+            continue
