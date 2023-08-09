277d73a64fe3ba51f03b763a25e7ff45e9b3ab40
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-08-09 06:09:57 +0100

    edited to pycodestyle

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 i = 1
-for char in range (ord('z'), ord('a') - 1, -1):
+for char in range(ord('z'), ord('a') - 1, -1):
     if (i % 2 == 0):
         char = char - 32
     print("{}".format(chr(char)), end='')
