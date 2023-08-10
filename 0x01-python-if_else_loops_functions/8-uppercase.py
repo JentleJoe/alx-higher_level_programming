7ff7991abbcd1058a9424c87a39a7474745ea255
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-08-10 06:01:58 +0100

    handles empty string

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(0, len(str)):
+        if (len(str) == 0):
+            break
         if (ord(str[i]) >= 97 and ord(str[i]) <= 122):
             char = chr(ord(str[i]) - 32)
         else:
