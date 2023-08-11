7185279387c612957874a0789b1b1b55b08b5c1b
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-08-11 15:45:20 +0100

    added new file

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    if (len(argv) < 4 or len(argv) > 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    sign = argv[2]
+    if (sign != '+' and sign != '-' and sign != '*' and sign != '/'):
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+
+    arr = ['+', '-', '*', '/']
+    if (argv[2] == arr[0]):
+        print("{} {} {} = {}".format(a, sign, b, add(a, b)))
+    if (argv[2] == arr[1]):
+        print("{} {} {} = {}".format(a, sign, b, sub(a, b)))
+    if (argv[2] == arr[2]):
+        print("{} {} {} = {}".format(a, sign, b, mul(a, b)))
+    if (argv[2] == arr[3]):
+        print("{} {} {} = {}".format(a, sign, b, div(a, b)))
