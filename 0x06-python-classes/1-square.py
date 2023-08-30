e27017c8f6d23fcc5c5e89c67b1fedd6f339648e
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-08-30 13:53:06 +0100

    updated source code

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,6 +6,10 @@ class Square:
     """ A square Class """
 
     def __init__(self, size):
-        """ Initializes the square class """
+        """ Initializes the square class
 
+        Args:
+            __size: the size of the square, it is met to be private
+
+        """
         self.__size = size
