eb68ca9f3a4432ef43785ba4d63717024e117ed5
JentleJoe <aigbokhaebholojoshua@gmail.com>
2023-08-30 04:23:13 +0100

    added new file

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" defines a square """
+
+
+class Square:
+    """ A square Class """
+
+    def __init__(self, size):
+        """ Initializes the square class """
+
+        self.__size = size
